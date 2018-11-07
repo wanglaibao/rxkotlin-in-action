c7dfe203f9fbc4c1e65c06f3f4e95279965c7da3
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-07 15:45:53 +0800

    update pom.xml file info

---
+++ rxjava-fundamental/src/main/java/com/laibao/rxjava/fundamental/reactiveextensions/NaturalNumberIterator.java
@@ -0,0 +1,23 @@
+package com.laibao.rxjava.fundamental.reactiveextensions;
+
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * @author laibao wang
+ */
+public class NaturalNumberIterator implements Iterator<BigInteger>{
+
+    private BigInteger currentValue = BigInteger.ZERO;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public BigInteger next() {
+        return currentValue.add(BigInteger.ONE);
+    }
+}
